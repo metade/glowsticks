cbe07f4d877e4e6c5697f20c355d1abde3e71180
Andrew Hilton <hiltoa02@MC-S001890.local>
2010-09-04 19:04:23 +0100

    jquery sqapped out fully

--- www/js/mice.js
+++ www/js/mice.js
@@ -1,25 +1,19 @@
 io.setPath('/js/socket/');
 
 get = glow.dom.get;
-function ratelimit(fn, ms) {
-  var last = (new Date()).getTime();
-  return (function() {
-    var now = (new Date()).getTime();
-    if (now - last > ms) {
-      last = now;
-      fn.apply(null, arguments);
-    }
-
-  });
+function throttle(method,e, scope) {
+    clearTimeout(method._tId);
+    method._tId= setTimeout(function(){
+        method(e)
+    }, 40);
 }
-
 function move(mouse){
   if(disabled == false){                               
     if(get('#mouse_'+mouse['id']).length == 0) {
       get('body').append('<span class="mouse" id="mouse_'+mouse['id']+'"><span style="display:none;" class="chat"/></span>');
     }
     get('#mouse_'+mouse['id']).css({
-      'left' : (($(window).width() - mouse['w']) / 2 + mouse['x']) + 'px',
+      'left' : ((get(window).width() - mouse['w']) / 2 + mouse['x']) + 'px',
       'top' : mouse['y'] + 'px'
     })
   }
@@ -31,11 +25,11 @@ function speak(data){
   get('#mouse_'+data['id']).append('<img src="http://www.gravatar.com/avatar/' + data['email'] + '?s=20" />');
 
   if(data['text'] == '') {
-    return $('#mouse_'+data['id']+' .chat').hide();
+    return get('#mouse_'+data['id']+' .chat').hide();
   }
 
-  $('#mouse_'+data['id']+' .chat').show().html(data['text']);
-  timeouts[data['id']] = setTimeout("$('#mouse_"+data['id']+" .chat').hide()", 5000)
+  get('#mouse_'+data['id']+' .chat').show().html(data['text']);
+  timeouts[data['id']] = setTimeout("get('#mouse_"+data['id']+" .chat').hide()", 5000)
 };
 
 function preview(data){
@@ -49,7 +43,7 @@ function preview(data){
 
   get('#preview').show();
   get('#preview .chat').show().html(data['text']);
-  timeouts['preview'] = setTimeout("$('#preview').hide()", 5000)
+  timeouts['preview'] = setTimeout("get('#preview').hide()", 5000)
 };
 
 
@@ -73,20 +67,18 @@ function preview(data){
 document.body.onmousemove =  function (e) {
   trail.moveIt(e);
   
-  ratelimit(function(e){
-    socket.send(JSON.stringify({
+  var send = function(e){
+    console.log('BOO');
+    socket.send(glow.data.encodeJson({
       action: 'move',
       x: e.pageX,
       y: e.pageY,
       w: get(window).width(),
       h: get(window).height()
-    }))
+    }))};
+throttle(send,e,window);
 
-    $('#preview').css({
-      'left' : e.pageX + 'px',
-      'top' : e.pageY + 'px'
-    })
-  }, 40)
+
 }
 
 
@@ -98,9 +90,10 @@ var disabled = false,
 
 if(socket.connect()){
   socket.on('message', function(data){
+    console.log(data);
     data = glow.data.decodeJson(data); 
     if(data['action'] == 'close'){
-      $('#mouse_'+data['id']).remove();
+      get('#mouse_'+data['id']).remove();
     } else if(data['action'] == 'speak') {
       if(data['id']) {
         speak(data);
