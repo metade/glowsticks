2c8dcb25100fedc61cd98fbead923adb8f649626
Andrew Hilton <hiltoa02@MC-S001890.local>
2010-09-04 20:30:27 +0100

    glowsticktastic

--- www/js/mice.js
+++ www/js/mice.js
@@ -10,41 +10,19 @@ function throttle(method,e, scope) {
 function move(mouse){
   if(disabled == false){                               
     if(get('#mouse_'+mouse['id']).length == 0) {
-      get('body').append('<span class="mouse" id="mouse_'+mouse['id']+'"><span style="display:none;" class="chat"/></span>');
+      var cssClass = 'color' + parseInt(Math.random()*4);
+      get('body').append('<span class="mouse ' +  cssClass + '" id="mouse_'+mouse['id']+'"><span style="display:none;" class="chat"/></span>');
     }
     get('#mouse_'+mouse['id']).css({
       'left' : ((get(window).width() - mouse['w']) / 2 + mouse['x']) + 'px',
       'top' : mouse['y'] + 'px'
     })
+    trail({pageX: mouse['x'],pageY: mouse['y'] });
   }
 };
 
-function speak(data){
-  clearTimeout(timeouts[data['id']]);
-  get('#mouse_'+data['id']+' img').remove();
-  get('#mouse_'+data['id']).append('<img src="http://www.gravatar.com/avatar/' + data['email'] + '?s=20" />');
 
-  if(data['text'] == '') {
-    return get('#mouse_'+data['id']+' .chat').hide();
-  }
 
-  get('#mouse_'+data['id']+' .chat').show().html(data['text']);
-  timeouts[data['id']] = setTimeout("get('#mouse_"+data['id']+" .chat').hide()", 5000)
-};
-
-function preview(data){
-  clearTimeout(timeouts[data['preview']]);
-  get('#preview img').remove();
-  get('#preview').append('<img src="http://www.gravatar.com/avatar/' + data['email'] + '?s=20" />');
-
-  if(data['text'] == '') {
-    return get('#preview .chat').hide();
-  }
-
-  get('#preview').show();
-  get('#preview .chat').show().html(data['text']);
-  timeouts['preview'] = setTimeout("get('#preview').hide()", 5000)
-};
 
 
  glow.ready(function(){
@@ -68,7 +46,6 @@ document.body.onmousemove =  function (e) {
   trail.moveIt(e);
   
   var send = function(e){
-    console.log('BOO');
     socket.send(glow.data.encodeJson({
       action: 'move',
       x: e.pageX,
@@ -90,7 +67,6 @@ var disabled = false,
 
 if(socket.connect()){
   socket.on('message', function(data){
-    console.log(data);
     data = glow.data.decodeJson(data); 
     if(data['action'] == 'close'){
       get('#mouse_'+data['id']).remove();
--- www/js/trail.js
+++ www/js/trail.js
@@ -1,5 +1,6 @@
  trail = {
   moveIt: function(e){
+    console.log(e);
     var seed = parseInt(Math.random()*100); 
     setTimeout(("trail.removeit(" + seed + ")"),400);
     get('body').append('<div class="trail" id="unique'+ seed+ '" style="background:'+this.randomColour()+';left:' + e.pageX + 'px;top:' + e.pageY + 'px;">&nbsp;</div>');   
